44371a29af0db3126df4433f7ed8b7585a9123a7
Kevin Lewis <kvn@lws.io>
2021-07-07 12:10:41 +0100

    Full test coverage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 const pkg = require('../package.json')
-const create = require('./crud/create')
-const read = require('./crud/read')
-const update = require('./crud/update')
-const destroy = require('./crud/destroy')
+const axios = require('axios')
+const qs = require('querystring')
 
 class OrbitActivities {
 	constructor(orbitWorkspaceId, orbitApiKey, userAgent) {
@@ -10,8 +8,9 @@ class OrbitActivities {
 	}
 
     setCredentials(orbitWorkspaceId, orbitApiKey, userAgent) {
-        if(!(orbitWorkspaceId || process.env.ORBIT_WORKSPACE_ID)) throw new Error('You must provide an Orbit Workspace ID when initializing Orbit or by setting an ORBIT_WORKSPACE_ID environment variable')
-        if(!(orbitApiKey || process.env.ORBIT_API_KEY)) throw new Error('You must provide an Orbit API Key when initializing Orbit or by setting an ORBIT_API_KEY environment variable')
+        if (!(orbitWorkspaceId || process.env.ORBIT_WORKSPACE_ID) || !(orbitApiKey || process.env.ORBIT_API_KEY)) {
+            throw new Error('You must provide and Orbit Workspace ID and Orbit API Key')
+        }
         return {
             orbitWorkspaceId: orbitWorkspaceId || process.env.ORBIT_WORKSPACE_ID,
             orbitApiKey: orbitApiKey || process.env.ORBIT_API_KEY,
@@ -19,43 +18,136 @@ class OrbitActivities {
         }
     }
 
-    listWorkspaceActivities(query = {}) {
-        return read.workspaceActivities(this, query)
-    }
-
-    listMemberActivities(memberId, query = {}) {
-        if(!memberId) throw new Error('You must provide a memberId as the first parameter')
-        if(typeof memberId !== 'string') throw new Error('memberId must be a string')
-        return read.memberActivities(this, memberId, query)
-    }
-
-    getActivity(activityId) {
-        if(!activityId) throw new Error('You must provide an activity id')
-        return read.activity(this, activityId)
-    }
-
-    createActivity(memberId, data) {
-        if(typeof memberId === 'object') data = memberId
-        if(!data) throw new Error('You must provide a payload when creating activities')
-        if(typeof memberId === 'string') {
-            return create.activityByMember(this, memberId, data)
-        } else {
-            return create.activity(this, data)
+    async listWorkspaceActivities(query = {}) {
+        try {
+            const response = await this.api(this.credentials, 'GET', '/activities', query)
+            const nextPage = getNextPageFromURL(response.links.next)
+            return {
+                data: response.data,
+                included: response.included,
+                items: response.data.length,
+                nextPage
+            }
+        } catch(error) {
+            throw new Error(error)
         }
     }
 
-    updateActivity(memberId, activityId, data) {
-        if(!memberId) throw new Error('You must provide a memberId as the first parameter')
-        if(!activityId) throw new Error('You must provide an activityId as the second parameter')
-        if(!data) throw new Error('You must provide a data object as the third parameter')
-        return update.activity(this, memberId, activityId, data)
+    async listMemberActivities(memberId, query = {}) {
+        try {
+            if(!memberId) throw new Error('You must provide a memberId')
+            if(typeof memberId !== 'string') throw new Error('memberId must be a string')
+
+            const response = await this.api(this.credentials, 'GET', `/members/${memberId}/activities`, query)
+            const nextPage = getNextPageFromURL(response.links.next)
+
+            return {
+                data: response.data,
+                included: response.included,
+                items: response.data.length,
+                nextPage
+            }
+
+        } catch(error) {
+            throw new Error(error)
+        }
     }
 
-    deleteActivity(memberId, activityId) {
-        if(!memberId) throw new Error('You must provide a memberId as the first parameter')
-        if(!activityId) throw new Error('You must provide an activityId as the second parameter')
-        return destroy.activity(this, memberId, activityId)
+    async getActivity(activityId) {
+        try {
+            if(!activityId) throw new Error('You must provide an activityId')
+            const response = await this.api(this.credentials, 'GET', `/activities/${activityId}`)
+            return response
+        } catch(error) {
+            throw new Error(error)
+        }
     }
+
+    async createActivity(memberId, data) {
+        if(memberId && data) {
+            return this.createActivityByMemberId(memberId, data)
+        } else {
+            data = memberId
+            return this.createActivityForUnknownMember(data)
+        }
+    }
+
+    async createActivityByMemberId(memberId, data) {
+        try {
+            if(!memberId || !data) throw new Error('You must provide a memberId and data')
+            if(typeof memberId !== 'string') throw new Error('memberId must be a string')
+            if(typeof data !== 'object') throw new Error('data must be an object')
+            const response = await this.api(this.credentials, 'POST', `/members/${memberId}/activities`, null, data)
+            return response
+        } catch(error) {
+            throw new Error(error)
+        }
+    }
+
+    async createActivityForUnknownMember(data) {
+        try {
+            if(!data) throw new Error('You must provide data')
+            if(typeof data !== 'object') throw new Error('data must be an object')
+            const response = await this.api(this.credentials, 'POST', '/activities', null, data)
+            return response
+        } catch(error) {
+            throw new Error(error)
+        }
+    }
+
+    async updateActivity(memberId, activityId, data) {
+        try {
+            if(!memberId) throw new Error('You must provide a memberId as the first parameter')
+            if(!activityId) throw new Error('You must provide an activityId as the second parameter')
+            if(!data) throw new Error('You must provide a data object as the third parameter')
+            await this.api(this.credentials, 'PUT', `/members/${memberId}/activities/${activityId}`, null, data)
+            return `activity ${activityId} on member ${memberId} was updated`
+        } catch(error) {
+            throw new Error(error)
+        }
+        // return update.activity(this, memberId, activityId, data)
+    }
+
+    // updateActivity(memberId, activityId, data) {
+    //     if(!memberId) throw new Error('You must provide a memberId as the first parameter')
+    //     if(!activityId) throw new Error('You must provide an activityId as the second parameter')
+    //     if(!data) throw new Error('You must provide a data object as the third parameter')
+    //     return update.activity(this, memberId, activityId, data)
+    // }
+
+    // deleteActivity(memberId, activityId) {
+    //     if(!memberId) throw new Error('You must provide a memberId as the first parameter')
+    //     if(!activityId) throw new Error('You must provide an activityId as the second parameter')
+    //     return destroy.activity(this, memberId, activityId)
+    // }
+
+    async api(credentials, method, endpoint, query = {}, data = {}) {
+        try {
+            if(!credentials || !method || !endpoint) throw new Error('You must pass a client, method, and endpoint')
+
+            const response = await axios({
+                method,
+                url: `https://app.orbit.love/api/v1/${credentials.orbitWorkspaceId}${endpoint}?${qs.encode(query)}`,
+                data,
+                headers: {
+                    Authorization: `Bearer ${credentials.orbitApiKey}`,
+                    'User-Agent': credentials.userAgent
+                }
+            })
+
+            return response?.data
+        } catch(err) {
+            throw new Error(err)
+        }
+    }
+
+}
+
+function getNextPageFromURL(url) {
+    if(!url) return null
+    const search = url.split('?')[1]
+    const page = +qs.decode(search).page
+    return page
 }
 
 module.exports = OrbitActivities
---
+++ src/index.test.js
@@ -0,0 +1,441 @@
+const OrbitActivities = require('../src/index.js')
+const axios = require('axios')
+const url = require('url')
+
+jest.mock('axios')
+
+describe('OrbitActivities constructor', () => {
+  it('given all credentials, does not throw', () => {
+    const sut = new OrbitActivities('1', '2')
+    expect(sut).not.toBeNull()
+  })
+
+  it('given missing configuration values, throws', () => {
+    delete process.env.ORBIT_WORKSPACE_ID
+    delete process.env.ORBIT_API_KEY
+
+    expect(() => {
+      new OrbitActivities(null, null)
+    }).toThrow()
+  })
+
+  it('configuration read from env variables when not directly provided', () => {
+    process.env.ORBIT_WORKSPACE_ID = '1'
+    process.env.ORBIT_API_KEY = '2'
+
+    const sut = new OrbitActivities(null, null)
+
+    expect(sut.credentials.orbitWorkspaceId).toBe('1')
+    expect(sut.credentials.orbitApiKey).toBe('2')
+  })
+
+  it('given no user agent, the default is set correctly', () => {
+    const sut = new OrbitActivities('1', '2')
+    expect(sut.credentials.userAgent).toContain('js-orbit-activities/')
+  })
+
+  it('given a user agent, it is set correctly', () => {
+    const sut = new OrbitActivities('1', '2', '3')
+    expect(sut.credentials.userAgent).toBe('3')
+  })
+})
+
+describe('OrbitActivities api', () => {
+  let sut
+  beforeEach(() => {
+    sut = new OrbitActivities('1', '2')
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  it('missing all required parameters, throws', async () => {
+    await expect(sut.api()).rejects.toThrow(
+      'You must pass a client, method, and endpoint'
+    )
+  })
+
+  it('missing some required parameters, throws', async () => {
+    await expect(sut.api('1', '2')).rejects.toThrow(
+      'You must pass a client, method, and endpoint'
+    )
+  })
+
+  it('calls axios correctly based on params', async () => {
+    const toReturn = { data: [{ key: 'value' }] }
+    axios.mockResolvedValueOnce(toReturn)
+
+    await sut.api(
+      sut.credentials,
+      '3',
+      '/4',
+      { queryKey: 'queryValue' },
+      { dataKey: 'dataValue' }
+    )
+
+    const firstCall = axios.mock.calls[0][0]
+
+    expect(axios).toHaveBeenCalledTimes(1)
+    expect(firstCall.headers.Authorization).toBe('Bearer 2')
+    expect(firstCall.method).toBe('3')
+    expect(firstCall.data.dataKey).toBe('dataValue')
+    expect(url.parse(firstCall.url, true).query.queryKey).toBe('queryValue')
+  })
+})
+
+describe('OrbitActivities listWorkspaceActivities', () => {
+  let sut
+  beforeEach(() => {
+    sut = new OrbitActivities('1', '2')
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  it('returns data in correct format', async () => {
+    const toReturn = setActivitiesResponse({ nextPage: 3 })
+    axios.mockResolvedValueOnce(toReturn)
+
+    const response = await sut.listWorkspaceActivities()
+
+    expect(response.data.length).toBe(2)
+    expect(response.data.included).not.toBeNull()
+    expect(response.items).toBe(2)
+    expect(response.nextPage).toBe(3)
+  })
+
+  it('given nextPageUrl, provides nextPage integer, ', async () => {
+    const toReturn = setActivitiesResponse({ nextPage: 3 })
+    axios.mockResolvedValueOnce(toReturn)
+
+    const response = await sut.listWorkspaceActivities()
+
+    expect(response.nextPage).toBe(3)
+  })
+
+  it('given no nextPageUrl, sets nextPage to null, ', async () => {
+    const toReturn = setActivitiesResponse({ nextPage: null })
+    axios.mockResolvedValueOnce(toReturn)
+
+    const response = await sut.listWorkspaceActivities()
+
+    expect(response.nextPage).toBeNull()
+  })
+
+  it('when there is an error, return error', async () => {
+    const errorMessage = 'Network Error'
+    axios.mockImplementationOnce(() => {
+      return Promise.reject(new Error(errorMessage))
+    })
+
+    await expect(sut.listWorkspaceActivities()).rejects.toThrow(errorMessage)
+  })
+})
+
+describe('OrbitActivities listMemberActivities', () => {
+  let sut
+  beforeEach(() => {
+    sut = new OrbitActivities('1', '2')
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  it('if memberId is not provided, throws', async () => {
+    await expect(sut.listMemberActivities()).rejects.toThrow(
+      'You must provide a memberId'
+    )
+  })
+
+  it('if memberId is not a string, throws', async () => {
+    const errorText = 'memberId must be a string'
+    await expect(sut.listMemberActivities(123)).rejects.toThrow(errorText)
+    await expect(sut.listMemberActivities(true)).rejects.toThrow(errorText)
+    await expect(sut.listMemberActivities({})).rejects.toThrow(errorText)
+  })
+
+  it('returns data in correct format', async () => {
+    const toReturn = setActivitiesResponse({ nextPage: 3 })
+    axios.mockResolvedValueOnce(toReturn)
+
+    const response = await sut.listMemberActivities('123')
+
+    expect(response.data.length).toBe(2)
+    expect(response.data.included).not.toBeNull()
+    expect(response.items).toBe(2)
+    expect(response.nextPage).toBe(3)
+  })
+
+  it('given nextPageUrl, provides nextPage integer', async () => {
+    const toReturn = setActivitiesResponse({ nextPage: 4 })
+    axios.mockResolvedValueOnce(toReturn)
+
+    const response = await sut.listMemberActivities('123')
+
+    expect(response.nextPage).toBe(4)
+  })
+
+  it('given no nextPageUrl, sets nextPage to null', async () => {
+    const toReturn = setActivitiesResponse({ nextPage: null })
+    axios.mockResolvedValueOnce(toReturn)
+
+    const response = await sut.listMemberActivities('123')
+
+    expect(response.nextPage).toBeNull()
+  })
+
+  it('when there is an error, return error', async () => {
+    const errorMessage = 'Network Error'
+    axios.mockImplementationOnce(() => {
+      return Promise.reject(new Error(errorMessage))
+    })
+
+    await expect(sut.listMemberActivities('123')).rejects.toThrow(errorMessage)
+  })
+})
+
+describe('OrbitActivities getActivity', () => {
+  let sut
+  beforeEach(() => {
+    sut = new OrbitActivities('1', '2')
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  it('if activityId is missing, throws', async () => {
+    await expect(sut.getActivity()).rejects.toThrow(
+      'You must provide an activityId'
+    )
+  })
+
+  it('returns data in correct format', async () => {
+    const toReturn = { data: { data: { id: 'id-val' }, included: [] } }
+    axios.mockResolvedValueOnce(toReturn)
+
+    const response = await sut.getActivity('123')
+
+    expect(response.data.id).toBe('id-val')
+    expect(response.included).not.toBeNull()
+  })
+
+  it('when there is an error, return error', async () => {
+    const errorMessage = 'Network Error'
+    axios.mockImplementationOnce(() => {
+      return Promise.reject(new Error(errorMessage))
+    })
+
+    await expect(sut.getActivity('123')).rejects.toThrow(errorMessage)
+  })
+})
+
+describe('OrbitActivities createActivity', () => {
+  let sut
+  beforeEach(() => {
+    sut = new OrbitActivities('1', '2')
+  })
+
+  it('given one argument which is an object, call createActivityForUnknownMember', () => {
+    const spy = jest.spyOn(sut, 'createActivityForUnknownMember')
+    sut.createActivity({})
+    expect(spy).toHaveBeenCalledTimes(1)
+  })
+
+  it('given a memberId and an object, call createActivityByMemberId', () => {
+    const spy = jest.spyOn(sut, 'createActivityByMemberId')
+    sut.createActivity('123', {})
+    expect(spy).toHaveBeenCalledTimes(1)
+  })
+})
+
+describe('OrbitActivities createActivityByMemberId', () => {
+  let sut
+  beforeEach(() => {
+    sut = new OrbitActivities('1', '2')
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  it('if memberId or data is not provided, throws', async () => {
+    await expect(sut.createActivityByMemberId()).rejects.toThrow(
+      'You must provide a memberId and data'
+    )
+    await expect(sut.createActivityByMemberId(null, {})).rejects.toThrow(
+      'You must provide a memberId and data'
+    )
+    await expect(sut.createActivityByMemberId('123', null)).rejects.toThrow(
+      'You must provide a memberId and data'
+    )
+  })
+
+  it('if parameters are the wrong type, throws', async () => {
+    const stringError = 'memberId must be a string'
+    const objectError = 'data must be an object'
+    await expect(sut.createActivityByMemberId(123, {})).rejects.toThrow(
+      stringError
+    )
+    await expect(sut.createActivityByMemberId(true, {})).rejects.toThrow(
+      stringError
+    )
+    await expect(sut.createActivityByMemberId({}, {})).rejects.toThrow(
+      stringError
+    )
+    await expect(sut.createActivityByMemberId('string', 123)).rejects.toThrow(
+      objectError
+    )
+    await expect(sut.createActivityByMemberId('string', true)).rejects.toThrow(
+      objectError
+    )
+    await expect(
+      sut.createActivityByMemberId('string', 'string')
+    ).rejects.toThrow(objectError)
+  })
+
+  it('returns data in the correct format', async () => {
+    const toReturn = setActivitiesResponse()
+    axios.mockResolvedValueOnce(toReturn)
+    const response = await sut.createActivityByMemberId('123', {})
+    expect(response).toMatchObject(toReturn.data)
+  })
+
+  it('when there is an error, return error', async () => {
+    const errorMessage = 'Network Error'
+    axios.mockImplementationOnce(() => {
+      return Promise.reject(new Error(errorMessage))
+    })
+
+    await expect(sut.createActivityByMemberId('123', {})).rejects.toThrow(
+      errorMessage
+    )
+  })
+})
+
+describe('OrbitActivities createActivityForUnknownMember', () => {
+  let sut
+  beforeEach(() => {
+    sut = new OrbitActivities('1', '2')
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  it('if data is not provided, throws', async () => {
+    await expect(sut.createActivityForUnknownMember()).rejects.toThrow(
+      'You must provide data'
+    )
+  })
+
+  it('if data is not an object', async () => {
+    const errorText = 'data must be an object'
+    await expect(sut.createActivityForUnknownMember(123)).rejects.toThrow(
+      errorText
+    )
+    await expect(sut.createActivityForUnknownMember(true)).rejects.toThrow(
+      errorText
+    )
+    await expect(sut.createActivityForUnknownMember('string')).rejects.toThrow(
+      errorText
+    )
+  })
+
+  it('returns data in the correct format', async () => {
+    const toReturn = setActivitiesResponse()
+    axios.mockResolvedValueOnce(toReturn)
+    const response = await sut.createActivityForUnknownMember({})
+    expect(response).toMatchObject(toReturn.data)
+  })
+
+  it('when there is an error, return error', async () => {
+    const errorMessage = 'Network Error'
+    axios.mockImplementationOnce(() => {
+      return Promise.reject(new Error(errorMessage))
+    })
+
+    await expect(sut.createActivityForUnknownMember({})).rejects.toThrow(
+      errorMessage
+    )
+  })
+})
+
+describe('orbitActivities updateActivity', () => {
+  let sut
+  beforeEach(() => {
+    sut = new OrbitActivities('1', '2')
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  it('if parameters are missing, throws', async () => {
+    await expect(sut.updateActivity()).rejects.toThrow(
+      'You must provide a memberId as the first parameter'
+    )
+    await expect(sut.updateActivity('1')).rejects.toThrow(
+      'You must provide an activityId as the second parameter'
+    )
+    await expect(sut.updateActivity('1', '2')).rejects.toThrow(
+      'You must provide a data object as the third parameter'
+    )
+  })
+
+  it('calls axios correctly', async () => {
+    axios.mockResolvedValueOnce({})
+
+    await sut.updateActivity('123', '456', { dataKey: 'dataValue' })
+
+    const firstCall = axios.mock.calls[0][0]
+    const path = url
+      .parse(firstCall.url, true)
+      .path.split('v1')[1]
+      .split('?')[0]
+    const memberId = path.split('/')[3]
+    const activityId = path.split('/')[5]
+
+    expect(memberId).toBe('123')
+    expect(activityId).toBe('456')
+  })
+
+  it('returns success message correctly', async () => {
+    axios.mockResolvedValueOnce({})
+
+    const response = await sut.updateActivity('123', '456', {
+      dataKey: 'dataValue'
+    })
+
+    expect(response).toBe('activity 456 on member 123 was updated')
+  })
+
+  it('when there is an error, return error', async () => {
+    const errorMessage = 'Network Error'
+    axios.mockImplementationOnce(() => {
+      return Promise.reject(new Error(errorMessage))
+    })
+
+    await expect(sut.updateActivity('1', '2', {})).rejects.toThrow(errorMessage)
+  })
+})
+
+////////
+
+function setActivitiesResponse(params) {
+  const next = params?.nextPage
+    ? `https://app.orbit.love/api/v1/workspaceid/activities?filters=true&items=25&page=${params.nextPage}&sort=occurred_at`
+    : null
+  return {
+    data: {
+      data: [{}, {}],
+      included: [],
+      links: {
+        next
+      }
+    }
+  }
+}
